b1cce4d65ea49e497e2e45a9371f0bf3caf53fc3
NeoDyeBlue <57549644+NeoDyeBlue@users.noreply.github.com>
2020-02-18 15:55:56 +0700

    fonts, scales and placements

--- music player.py
+++ music player.py
@@ -19,23 +19,23 @@ class Player(tk.Frame):
         self.gui()
 
     def gui(self):
-        self.playFrame = LabelFrame(self.master, height = 125, width = 460, bg = 'gray45',borderwidth = 0)
-        self.playFrame.place(x = 0, y = 330)
+        self.playFrame = LabelFrame(self.master, height = 100, width = 460, bg = 'gray45',borderwidth = 0)
+        self.playFrame.place(x = 0, y = 340)
 
-        self.pl_time = Label(self.playFrame, text = '00:00', fg = 'white',bg = 'gray45', font =('calibri',11))
-        self.pl_time.place(x=100,y=3)
+        self.pl_time = Label(self.playFrame, text = '00:00', fg = 'white',bg = 'gray45', font =('calibri',8))
+        self.pl_time.place(x=90,y=5)
 
-        self.musicFrame = LabelFrame(self.playFrame, height = 89, width = 89,borderwidth = 0)
-        self.musicFrame.place(x = 5, y = 6)
+        self.musicFrame = LabelFrame(self.playFrame, height = 80, width = 80,borderwidth = 0)
+        self.musicFrame.place(x = 5, y = 5)
 
         self.music = Label(self.musicFrame, text = '♫',fg = 'orange', font = ('impact',40))
         self.music.place(x = 15, y = 3)
         
         self.play = Button(self.playFrame, text = self.PsePly, font = ('impact',20),fg = 'white', bg = 'gray45',
                            activeforeground = 'orange',activebackground = 'gray45',borderwidth = 0, command = self.pseply)
-        self.play.place(x = 245, y = 50)
+        self.play.place(x = 245, y = 40)
 
-        self.MusicList = LabelFrame(self.master, height = 320, width = 360, borderwidth = 0, bg = 'gray75')
+        self.MusicList = LabelFrame(self.master, height = 330, width = 360, borderwidth = 0, bg = 'gray75')
         self.MusicList.place(x = 100, y = 0)
 
         self.dirctry = LabelFrame(self.master, height = 30, width = 350, bg = 'gray45', borderwidth = 0)
@@ -45,11 +45,11 @@ class Player(tk.Frame):
                             activeforeground = 'orange', activebackground = 'gray45', borderwidth = 0,command = self.askdirctry)
         self.foldr.place(x = 1, y = -17)
 
-        self.fldrnme = Label(self.dirctry, text = '← Choose a directory to look for .mp3 and .wav files.', fg = 'white', bg = 'gray45')
-        self.fldrnme.place(x = 40, y = 4)
+        self.fldrnme = Label(self.dirctry, text = '← Choose a directory to look for .mp3 and .wav files.',font =('calibri',8), fg = 'white', bg = 'gray45')
+        self.fldrnme.place(x = 40, y = 5)
 
-        self.nomusic = Label(self.MusicList, text = 'Please choose a directory first', font = 15, fg = 'white', bg = 'gray75')
-        self.nomusic.place(x = 70, y = 155)
+        self.nomusic = Label(self.MusicList, text = 'Please choose a directory first', font =('calibri',12), fg = 'white', bg = 'gray75')
+        self.nomusic.place(x = 78, y = 155)
 
     def nothing(self):
         pass
@@ -64,8 +64,8 @@ class Player(tk.Frame):
 
     def askdirctry(self):
         self.folderop = filedialog.askdirectory()
-        if len(self.folderop) > 47:
-            self.fldrnme.config(text = '← ' + self.folderop[0:47]+'...')
+        if len(self.folderop) > 55:
+            self.fldrnme.config(text = '← ' + self.folderop[0:55]+'...')
         elif len(self.folderop) == 0:
             self.fldrnme.config(text = '← Choose a directory to look for .mp3 and .wav files.')
         else:
